a1b47dcd26922ac927b6e10934f40b134c008277
Denis <kabach.dan@gmail.com>
2018-11-30 22:30:11 +0600

    rewritten mocks.js

--- mocks.js
+++ mocks.js
@@ -1,19 +1,26 @@
 const faker = require('faker');
-const TurndownService = require('turndown');
+const tr = require('transliter');
 
 const models = require('./models');
 
 const owner = '5bf7ee64d32bba2508dd37f2';
 
-module.exports = () => {
-  models.Post.remove().then(() => {
-    Array.from({ length: 20 }).forEach(() => {
-      const turndownService = new TurndownService();
-      models.Post.create({
-        title: faker.lorem.words(5),
-        body: turndownService.turndown(faker.lorem.words(100)),
+module.exports = async () => {
+  try {
+    await models.Post.remove();
+
+    Array.from({ length: 20 }).forEach(async () => {
+      const title = faker.lorem.words(5);
+      const url = `${tr.slugify(title)}-${Date.now().toString(36)}`;
+      const post = await models.Post.create({
+        title,
+        body: faker.lorem.words(100),
+        url,
         owner
-      }).then(console.log).catch(console.log)
-    })
-  }).catch(console.log)
-}
+      });
+      console.log(post);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
